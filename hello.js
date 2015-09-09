77edfa5673f89b5799e869f0992a056d1d629c13
bulrush15 <bacca400@comcast.net>
2015-09-09 10:17:20 -0400

    Initial create.

---
+++ hello.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Hello World
+// @namespace   bulrush
+// @description O'Reilly Hello World example
+// 
+// Execute this script on all sites but oreilly.com and www.oreilly.com. 
+// @include     *
+// @exclude     http://oreilly.com/*
+// @exclude     http://www.oreilly.com/*
+// @exclude     http://github.com/*
+// @exclude     http://www.github.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+alert('Hello world');
